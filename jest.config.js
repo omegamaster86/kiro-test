9394bfcd1807ac0e1ecd11144993a5dd5afbad97
omegamaster86 <masya868686@gmail.com>
2025-07-19 18:11:04 +0900

    update

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
